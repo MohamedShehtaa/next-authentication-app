27e574199affd25bb612b9f7da5600837ccd7660
MohamedShehtaa <mmshehta92@gmail.com>
2023-07-04 05:58:24 +0300

    clean our app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-import UserCard from '@/components/userCard/UserCard';
-
-const getData = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  return await res.json();
-};
-
 export default async function Home() {
-  const data = await getData();
-  return (
-    <div>
-      {data.map((user: any) => (
-        <UserCard
-          user={user}
-          key={user.id}
-        />
-      ))}
-    </div>
-  );
+  return <div>home</div>;
 }
