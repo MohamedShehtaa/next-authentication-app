3692bee5b1b455fde44004720ec358a65772e72a
Mohamed Shehta <102837319+MohamedShehtaa@users.noreply.github.com>
2023-08-13 14:40:36 +0300

    feat(api) : working with api routes  (#4)
    * feat(user): add first route handler function GET and deal with query params
    
    * feat(api): structure the api folder
    
    * refactor: add  missing async
    
    * feat(api) : add POST function for post request

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  try {
+    const queryParams = new URL(request.nextUrl);
+    console.log(queryParams.searchParams.get('user'));
+    return NextResponse.json({
+      message: 'users get request accessed successfully',
+      data: {
+        name: 'mohamed',
+        email: 'test@test.com',
+      },
+    });
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
