af1893b6effccc36da455cb106dd006c21f5fd68
MohamedShehtaa <mmshehta92@gmail.com>
2023-08-16 14:57:19 +0300

    get the user with help of token and return user to home page

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,18 +1,18 @@
 // i want to display welcome user if he logged in so we will use the token to get userId and get the user to display
 import { NextRequest, NextResponse } from 'next/server';
 import { validateTokenAndGetUser } from '@/utlis/tokenValidation';
+import User from '@/models/userModel';
+import { connectDb } from '@/config/dbConfig';
+
+connectDb();
 
 export async function GET(request: NextRequest) {
   try {
     const userId = await validateTokenAndGetUser(request);
-    const queryParams = new URL(request.nextUrl);
-    // console.log(queryParams.searchParams.get('user'));
+    const user = await User.findOne({ _id: userId }).select('-password');
     return NextResponse.json({
-      message: 'users get request accessed successfully',
-      data: {
-        name: 'mohamed',
-        email: 'test@test.com',
-      },
+      message: 'get user successfully',
+      data: user,
     });
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import axios from 'axios';
+import { cookies } from 'next/dist/client/components/headers';
 
 export async function getUser() {
   try {
-    // here we need to write the full url cuze is running in the server side
-    const response = await axios.get('http://localhost:3000/api/users/me');
-    return response.data;
+    // here we need to write the full url cuze is running in the server side and pass the cookies in the request
+    const response = await axios.get('http://localhost:3000/api/users/me', {
+      headers: {
+        cookie: `token=${cookies().get('token')?.value}`,
+      },
+    });
+    return response.data.data;
   } catch (e) {
     console.log(e);
   }
 }
 
 export default async function Home() {
-  const user = getUser();
-  return <div>home</div>;
+  const user: any = await getUser();
+  return <>{user && <div>hello {user.userName}</div>}</>;
 }
