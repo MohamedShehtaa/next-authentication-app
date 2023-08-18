2343d4f102b0c074afab666501eee6a2c726b4aa
MohamedShehtaa <mmshehta92@gmail.com>
2023-08-18 20:06:36 +0300

    update new routes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,16 +9,17 @@ export function middleware(request: NextRequest) {
   // specify the path to run it to specific route
   const path = request.nextUrl.pathname;
   const isPublicPage = path === '/login' || path === '/register';
+  const isVerificationPage = path === '/verifyemail' || path === '/resetpassword';
   const token = request.cookies.get('token')?.value || '';
   // => scenario's
 
   // A) if the path is public and the token is present , redirect to home page
-  if (isPublicPage && token) {
+  if (isPublicPage && token && !isVerificationPage) {
     return NextResponse.redirect(new URL('/', request.nextUrl));
   }
 
   // B) if the path is private and the no token  , redirect to login page
-  if (!isPublicPage && !token) {
+  if (!isPublicPage && !token && !isVerificationPage) {
     return NextResponse.redirect(new URL('/login', request.nextUrl));
   }
 
@@ -29,5 +30,5 @@ export function middleware(request: NextRequest) {
 // the previous will work correctly but the styles and assets does not work because we need to match the other  paths
 
 export const config = {
-  matcher: ['/', '/profile/:path*', '/login', '/register'],
+  matcher: ['/', '/profile/:path*', '/login', '/register', '/verifyemail', '/resetpassword'],
 };
